f2e70a33dfdc2ac8aeae137375a3eec19aa7b226
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-14 05:20:27 +0900

    ルーティングの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root   "users#new"
+  get    "/signup", to: "users#new"
+  post   "/signup", to: "users#create"
+  get    "/login",  to: "sessions#new"
+  post   "/login",  to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:show]
+  resources :account_activations, only: [:edit]
 end
