29b848514338c0715bb21b55cf6af784e31cc5ac
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-14 05:32:55 +0900

    Userモデルにアカウント承認メソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,45 @@
 class User < ApplicationRecord
+  attr_accessor :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   authenticates_with_sorcery!
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,    presence: true, length: { maximum: 255 },
+                       format: { with: VALID_EMAIL_REGEX },
+                       uniqueness: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
