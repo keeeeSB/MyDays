6a941988e94db3601ea7b4ad9e100e72cfee4514
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-15 05:10:54 +0900

    not_authenticatedメソッドの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
+
+  private
+
+    def not_authenticated
+      flash[:danger] = "ログインしてください。"
+      redirect_to root_path
+    end
 end
