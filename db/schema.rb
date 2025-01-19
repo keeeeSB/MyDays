71f9d4351c2173b7f4828348bb1d5287ddc34da4
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-19 14:18:04 +0900

    Diaryモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_19_045200) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_19_050627) do
+  create_table "diaries", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.datetime "written_on", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_diaries_on_user_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -31,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_19_045200) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "diaries", "users"
 end
