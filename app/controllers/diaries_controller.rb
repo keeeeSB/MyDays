ab1a1d0371e6629a5c532028aad9332ccab8a850
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-20 12:17:35 +0900

    Diariesコントローラーにアクションを追加

---
+++ app/controllers/diaries_controller.rb
@@ -0,0 +1,22 @@
+class DiariesController < ApplicationController
+
+  def new
+    @diary = current_user.diaries.build
+  end
+
+  def create
+    @diary = current_user.diaries.build(diary_params)
+    if @diary.save
+      
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+    def diary_params
+      params.require(:diary).permit(:tutle, :content, :written_on)
+    end
+end
