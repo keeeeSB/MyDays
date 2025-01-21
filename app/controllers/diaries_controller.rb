6779ba0e34b5b2407062fdd5922a2a8be5b49a29
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-21 09:57:44 +0900

    Diariesコントローラーにアクションを追加

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -7,11 +7,34 @@ class DiariesController < ApplicationController
   def create
     @diary = current_user.diaries.build(diary_params)
     if @diary.save
-      
+      flash[:success] = "今日の日記を投稿しました。"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "日記を投稿できませんでした。"
+      render :new, status: :unprocessable_entity
     end
   end
 
   def edit
+    @diary = current_user.diaries.find(params[:id])
+  end
+
+  def update
+    @diary = current_user.diaries.find(params[:id])
+    if @diary.update(diary_params)
+      flash[:success] = "日記を更新しました。"
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+
+  end
+
+  def destroy
+    @diary = current_user.diaries.find(params[:id])
+    @diary.destroy!
+    flash[:success] = "日記を削除しました。"
+    redirect_to root_path, status: :see_other
   end
 
   private
