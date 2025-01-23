63b65ba3147ac3f5592fcc233c55dac7157f5318
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-23 09:40:11 +0900

    日記一覧ページの作成

--- app/helpers/diaries_helper.rb
+++ app/helpers/diaries_helper.rb
@@ -1,2 +1,6 @@
 module DiariesHelper
+
+  def formatted_date(diary)
+    diary.written_on.strftime("%Y年%m月%d日")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root   "users#new"
+  root   "diaries#index"
   get    "/signup", to: "users#new"
   post   "/signup", to: "users#create"
   get    "/login",  to: "sessions#new"
