7c11dad369bb03baf2f32053c68d09b3ce3b3004
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-25 08:31:32 +0900

    root_pathを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root   "diaries#index"
+  root   "static_pages#home"
   get    "/signup", to: "users#new"
   post   "/signup", to: "users#create"
   get    "/login",  to: "sessions#new"
