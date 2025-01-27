fd8a7fa5e0cec253621c5390c7a49c6aa7820dfe
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-27 09:11:11 +0900

    Diaryモデルのテストを追加

--- spec/models/diary_spec.rb
+++ spec/models/diary_spec.rb
@@ -23,4 +23,44 @@ RSpec.describe Diary, type: :model do
       expect(diary).to_not be_valid
     end
   end
+
+  describe "タグ付けのテスト" do
+    it "複数のタグを日記に設定できる" do
+      diary.tag_list = "運動　スポーツ"
+      diary.save
+      expect(diary.tags.pluck(:name)).to contain_exactly("運動", "スポーツ")
+    end
+
+    it "重複したタグは一つだけ保存される" do
+      diary.tag_list = "運動　スポーツ　運動"
+      diary.save
+      expect(diary.tags.pluck(:name)).to contain_exactly("運動", "スポーツ")
+    end
+
+    describe "スペース区切りでタグを保存" do
+      context "半角スペースの場合" do
+        it "正しく保存できる" do
+          diary.tag_list = "運動 スポーツ 趣味"
+          diary.save
+          expect(diary.tags.pluck(:name)).to contain_exactly("運動", "スポーツ", "趣味")
+        end
+      end
+
+      context "全角スペースの場合" do
+        it "正しく保存できる" do
+          diary.tag_list = "運動　スポーツ　趣味"
+          diary.save
+          expect(diary.tags.pluck(:name)).to contain_exactly("運動", "スポーツ", "趣味")
+        end
+      end
+
+      context "半角、全角が混在する場合" do
+        it "正しく保存できる" do
+          diary.tag_list = "運動 スポーツ　趣味"
+          diary.save
+          expect(diary.tags.pluck(:name)).to contain_exactly("運動", "スポーツ", "趣味")
+        end
+      end
+    end
+  end
 end
